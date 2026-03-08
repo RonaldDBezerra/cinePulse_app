50ca6ccb2c67d51be590efc9828768e9483c33d0
Ronald Damasio <rebulgame@gmail.com>
2026-03-07 21:09:17 -0300

    style: add colors style

---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  background: "#0F0F0F",
+  card: "#1C1C1E",
+  primary: "#E50914",
+  text: "#FFFFFF",
+  textSecondary: "#B3B3B3",
+  white: "#FFFFFF",
+  textSecond: "#FFD700",
+};
